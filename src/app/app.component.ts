c51f610d8182736f44eff6b2d0b436a9134ce78b
Callum <callumgott@gmail.com>
2020-06-02 14:56:54 +1200

    Next section of commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Post }from 'src/app/posts/posts.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mean-Application';
+  storedPosts: Post[]  = [];
+
+  onPostAdded(post){
+    this.storedPosts.push(post);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,33 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+
   ],
   imports: [
     BrowserModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatExpansionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
